511b8486bf420f58dcb5ce8df29c8f9fd16dcbda
KunatsKaryna <97902168+KunatsKaryna@users.noreply.github.com>
2022-02-11 11:01:53 +0300

    Add files via upload

---
+++ pageobjects/login.page.js
@@ -0,0 +1,26 @@
+import Page from './page';
+const selectors = {
+  signinButton: '//*[@id="ybar-inner-wrap"]/div[3]/div/div[3]/div[1]/div/a',
+  loginField: '//*[@id="login-username"]',
+  nextButton: '//*[@id="login-signin"]',
+  passwordField: '//*[@id="login-passwd"]',
+  mailButton: '//*[@id="ybarMailLink"]/span[1]',
+  profileButton: '#ybarAccountMenuOpener',
+  logoffButton: '//*[@id="profile-signout-link"]',
+  };
+class LoginPage extends Page {
+  get signinButton() { return $(selectors.signinButton); }
+
+  get loginField() { return $(selectors.loginField); }
+
+  get nextButton() { return $(selectors.nextButton); }
+
+  get passwordField() { return $(selectors.passwordField); }
+
+  get mailButton() { return $(selectors.mailButton); }
+
+  get profileButton() { return $(selectors.profileButton); }
+  
+  get logoffButton() { return $(selectors.logoffButton); }
+  }
+export default new LoginPage();
---
+++ pageobjects/mail.page.js
@@ -0,0 +1,17 @@
+import Page from './page';
+const selectors = {
+  writeButton: '//*[@id="app"]/div[2]/div/div[1]/nav/div/div[1]/a',
+  whomField: '//*[@id="message-to-field"]',
+  themeField: '//input[@data-test-id="compose-subject"]',
+  bodyField: '//*[@id="editor-container"]/div[1]',
+  };
+class MailPage extends Page {  
+  get writeButton() { return $(selectors.writeButton); }
+
+  get whomField() { return $(selectors.whomField); }
+
+  get themeField() { return $(selectors.themeField); }
+   
+  get bodyField() { return $(selectors.bodyField); }
+  }
+export default new MailPage();
